04161d14e26d829c8d91f0c19e7d6bc3e8d89c92
Jakub Marciniszyn <lobin21@gmail.com>
2021-01-22 11:11:38 +0000

    Adds console table output

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const axios = require('axios');
 
 axios.defaults.headers.common['X-MBX-APIKEY'] = process.env.API_KEY // for POST requests
 
-const makeSignature = (data) => {
-    return crypto.createHmac('sha256', process.env.API_SECRET).update(data).digest('hex')
-}
-
 //public client
 const client = Binance()
 // Authenticated client, can make signed calls
@@ -18,10 +14,29 @@ const client2 = Binance({
     apiSecret: process.env.API_SECRET
 })
 
-const transferFromPoolToSpot = async () => {
+const output = [];
+
+//helper functions
+function makeSignature(data) {
+    return crypto.createHmac('sha256', process.env.API_SECRET).update(data).digest('hex')
+}
+
+function addToOutput(action, url, arr, result = true) {
+    arr.push({
+        Time: moment().format('D/MM/YYYY hh:mm:ss'),
+        Action: action,
+        Url: url,
+        Result: result ? 'PASS' : 'FAIL'
+    })
+}
+
+async function transferFromPoolToSpot() {
+    const functionName = arguments.callee.name;
+    const pathPaymentList = '/sapi/v1/mining/payment/list';
+    const pathAssetTransfer = '/sapi/v1/asset/transfer';
     let outstandingBalance = 0;
     try {
-        const {data: {accountProfits}} = await client2.privateRequest('GET', '/sapi/v1/mining/payment/list', {
+        const {data: {accountProfits}} = await client2.privateRequest('GET', pathPaymentList, {
             algo: 'ETHASH',
             userName: process.env.POOL_USERNAME,
             startDate: moment().subtract(1, 'days').valueOf()
@@ -33,25 +48,28 @@ const transferFromPoolToSpot = async () => {
             }
             return balance
         }, 0)
+        addToOutput(functionName, pathPaymentList, output);
     } catch (e) {
-        console.log(e)
+        addToOutput(functionName, pathPaymentList, output, false);
     }
     //if outstanding balance is greater than 0 transfer to spot wallet
     if (outstandingBalance > 0) {
         try {
-            const transfer = await client2.privateRequest('POST', '/sapi/v1/asset/transfer', {
+            const transfer = await client2.privateRequest('POST', pathAssetTransfer, {
                 type: 'MINING_MAIN',
                 asset: 'ETH',
                 amount: outstandingBalance.toString()
             })
-            console.log(transfer)
+            addToOutput(functionName, pathAssetTransfer, output);
         } catch (e) {
-            console.log(e)
+            addToOutput(functionName, pathAssetTransfer, output, false);
         }
     }
 }
 
-const buyBnb = async () => {
+async function buyBnb() {
+    const functionName = arguments.callee.name;
+    const pathOrder = '/api/v3/order';
     try {
         const {balances} = await client2.privateRequest('GET', '/api/v3/account');
         const gbpBalance = Number(balances.find(b => b['asset'] === 'GBP')['free']);
@@ -67,22 +85,24 @@ const buyBnb = async () => {
             });
             const qty = Number((gbpBalance / +BNBGBP) * (1 - fee / 100)).toFixed(precision);
             if (qty > 0) {
-                const buy = await client2.privateRequest('POST', '/api/v3/order', {
+                const buy = await client2.privateRequest('POST', pathOrder, {
                     symbol: 'BNBGBP',
                     side: 'BUY',
                     type: 'MARKET',
                     quantity: qty,
                     newOrderRespType: 'FULL'
                 })
-                console.log(buy)
+                addToOutput(functionName, pathOrder, output);
             }
         }
     } catch (e) {
-        console.log(e);
+        addToOutput(functionName, pathOrder, output, false);
     }
 }
 
-const convertDust = async () => {
+async function convertDust() {
+    const functionName = arguments.callee.name;
+    const pathAssetDust = '/sapi/v1/asset/dust';
     try {
         const params = new URLSearchParams();
         const {balances} = await client2.privateRequest('GET', '/api/v3/account');
@@ -95,11 +115,11 @@ const convertDust = async () => {
         params.append('timestamp', timestamp);
         const signature = makeSignature(params.toString());
         params.append('signature', signature);
-        const url = `${process.env.API_ENDPOINT}/sapi/v1/asset/dust?${params.toString()}`
+        const url = `${process.env.API_ENDPOINT}${pathAssetDust}?${params.toString()}`
         const convert = await axios.post(url);
-        console.log(convert.data);
+        addToOutput(functionName, pathAssetDust, output);
     } catch (e) {
-        console.log(e.response.data)
+        addToOutput(functionName, pathAssetDust, output, false);
     }
 }
 
@@ -108,6 +128,7 @@ const convertDust = async () => {
     await transferFromPoolToSpot();
     await buyBnb();
     await convertDust();
+    console.table(output)
 })();
 
 
